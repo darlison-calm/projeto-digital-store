cab67805774df79d63cc782f5242da88ae60b87c
darlison-calm <bernardo.darlison@gmail.com>
2024-10-17 16:54:35 -0300

    importar css color

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import BannerHome from "./components/Section/bannerHome";
+export default function App() {
+  return (
+    <div>
+      <BannerHome></BannerHome>
+    </div>
+  );
+}
---
+++ src/components/Section/bannerHome.jsx
@@ -0,0 +1,15 @@
+import '@globalStyles/reset.css';
+// import '@globalStyles/ColorsVariables.css';
+import './bannerHome.css';
+
+const BannerHome = () => {
+  return (
+    <>
+      <section className='bannerSection'>
+        <h2>334322</h2>
+      </section>
+    </>
+  )
+};
+
+export default BannerHome;
