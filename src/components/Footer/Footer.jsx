f020469c7be2e1928b09e159ea0e9caa50a7237d
Sir-Vinicius <viniciusidai@gmail.com>
2024-10-22 17:01:49 -0300

    Atualizar footer para tablet e desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,41 +10,45 @@ export function Footer() {
     return(
         <>
         <footer>
-            <div className='social'>
-                <img src={logo} alt="logo-digital-college" />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-                <div className='icons'>
-                    <a href="#"><img src={face} /></a>
-                    <a href="#"><img src={insta} /></a>
-                    <a href="#"><img src={x} /></a>
+            <div className='top'>
+                <div className='social'>
+                    <img className='logo' src={logo} alt="logo-digital-college" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                    <div className='icons'>
+                        <a href="#"><img src={face} /></a>
+                        <a href="#"><img src={insta} /></a>
+                        <a href="#"><img src={x} /></a>
+                    </div>
+                </div>
+                <div className='info-cat'>
+                    <div className='info'>
+                        <h3>Informação</h3>
+                        <a>Sobre Drip Store</a>
+                        <a>Segurança</a>
+                        <a>Wishlist</a>
+                        <a>Blog</a>
+                        <a>Trabalhe conosco</a>
+                        <a>Meus pedidos</a>
+                    </div>
+                    <div className='cat'>
+                        <h3>Categorias</h3>
+                        <a>Camisetas</a>
+                        <a>Calças</a>
+                        <a>Bonés</a>
+                        <a>Headphones</a>
+                        <a>Tênis</a>
+                    </div>
+                </div>
+                <div className='contato'>
+                    <h3>Contato</h3>
+                    <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+                    <p>(85) 3051-3411</p>
                 </div>
             </div>
-            <div className='info-cat'>
-                <div className='info'>
-                    <h3>Informação</h3>
-                    <a>Sobre Drip Store</a>
-                    <a>Segurança</a>
-                    <a>Wishlist</a>
-                    <a>Blog</a>
-                    <a>Trabalhe conosco</a>
-                    <a>Meus pedidos</a>
-                </div>
-                <div className='cat'>
-                    <h3>Categorias</h3>
-                    <a>Camisetas</a>
-                    <a>Calças</a>
-                    <a>Bonés</a>
-                    <a>Headphones</a>
-                    <a>Tênis</a>
-                </div>
+            <div className='direitos'>
+                <hr />
+                <p className='direitos'>@ 2022 Digital College</p>
             </div>
-            <div className='contato'>
-                <h3>Contato</h3>
-                <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
-                <p>(85) 3051-3411</p>
-            </div>
-            <hr />
-            <p className='direitos'>@ 2022 Digital College</p>
         </footer>
         </>
     );
