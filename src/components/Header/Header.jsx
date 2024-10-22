652f9710e23e7a13f11a66a15daa83f15eeb515a
darlison-calm <bernardo.darlison@gmail.com>
2024-10-22 15:07:17 -0300

    feat: criar header.style.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu, Search, ShoppingCart } from 'lucide-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './header.style.css'
 import logo from '../../assets/logo-header.svg'
 
 const Header = () => {
@@ -11,11 +12,11 @@ const Header = () => {
           className="btn btn-link p-0 me-2"
           aria-label="Menu"
         >
-          <Menu className="text-dark" size={30} />
+          <Menu className="text-dark" size={25} />
         </button>
 
-        <a className="navbar-brand m-0 text-danger fw-bold" href="/">
-            <img src={logo} alt="" className="w-60 w-md-50" />
+        <a className="navbar-brand m-0 text-danger fw-bold text-center" href="/">
+            <img src={logo} alt="" className="w-60"/>
         </a>
 
         <div className="d-flex align-items-center">
