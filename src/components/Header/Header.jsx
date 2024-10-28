90a8841689a6d5fad9ef7868a07ea80e9d353665
darlison-calm <bernardo.darlison@gmail.com>
2024-10-28 13:55:36 -0300

    fix:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,23 +11,23 @@ import {SearchBar} from './SearchBar';
 const Header = () => {
   return (
     <>
-      <header className="d-flex flex-column py-4">
-          {/* TOP */}
-            <div className='d-flex justify-content-around'>
-              <img src={logo} alt="" className='logo'/>
+      <header className="d-flex flex-column align-items-start">
+            <div className='d-flex justify-content-center top'>
+              <div className="d-flex flex-column gap-4">
+                <img src={logo} alt="" className='logo'/>
+                <div className="d-flex gap-4 mt-3">
+                  <a href="/" className="text-decoration-none">Home</a>
+                  <a href="/produtos" className="text-decoration-none">Produtos</a>
+                  <a href="/categorias" className="text-decoration-none">Categorias</a>
+                  <a href="/meus-pedidos" className="text-decoration-none">Meus Pedidos</a>
+                </div>
+              </div>
               <SearchBar className=""/>
-              <div className="d-flex">
+              <div className="d-flex justify-content-end gap-4">
                 <EntrarECadastroButtons button1Order={2} button2Order={0} className="gap-2"/>
-                <ShoppingCartButton/>
+                <ShoppingCartButton className="gap-4"/>
               </div>
             </div>
-          {/* BOTTOM */}
-          <div className="d-flex gap-4 links py-4">
-                <a href="/" className="text-decoration-none">Home</a>
-                <a href="/produtos" className="text-decoration-none">Produtos</a>
-                <a href="/categorias" className="text-decoration-none">Categorias</a>
-                <a href="/meus-pedidos" className="text-decoration-none">Meus Pedidos</a>
-          </div>
       </header>
     </>
   );
