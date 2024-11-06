8344b2f8c74e8aa360f486ddecf818dcd6929532
darlison-calm <bernardo.darlison@gmail.com>
2024-11-06 15:17:55 -0300

    feat/Product tListing

--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -1,5 +1,6 @@
 import { Product } from "../ProductCard/ProductCard";
 import { tenisDados } from '../../tenis.js';
+import arrow from '../../assets/long-arrow-right.svg'
 import '@globalStyles/ColorsVariables.css'
 import './ProductListing.css'
 
@@ -9,10 +10,16 @@ export function ProductListing() {
   return(
     <>
       <div className="products-container">
-        <div className="grid-container">
-          {tenisDados.slice(0, 8).map((tenisDados) => (
-              <Product props={tenisDados}/>
-            ))}
+        <div className="layout">
+          <div className="d-flex justify-content-between">
+            <h4>Produtos em destaque</h4>
+            <button>Ver todos <img src={arrow} className="right-arrow" alt="" srcset="" /></button>
+          </div>
+          <div className="grid-container">
+            {tenisDados.slice(0, 8).map((tenisDados) => (
+                <Product props={tenisDados}/>
+              ))}
+          </div>
         </div>
       </div>
 
