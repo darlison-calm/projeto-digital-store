5934164e10efb0a25f900ac9e69f3ed7bf43a2e6
Vinicius dos Santos <97066925+Sir-Vinicius@users.noreply.github.com>
2024-11-22 10:46:18 -0300

    Feat/header logoff (#22)
    * Adicionando código do menu do icone do usuario
    
    * Ajustando logoff

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,13 +7,30 @@ import { ShoppingCartButton } from './shoppingCart';
 import EntrarECadastroButtons from './EntrarECadastroButtons';
 import { SearchBar } from './SearchBar';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = ({
   showSearchBar = true,
   showShoppingCart = true,
   showHeader2 = true
 }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const userName = localStorage.getItem('userName');  // O nome do usuário está salvo aqui
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userName');
+    window.location.href = '/';
+  };
+
   return (
     <>
       <header className="d-flex flex-column">
@@ -22,10 +39,24 @@ const Header = ({
           {showSearchBar && <SearchBar className={"header1searchBar"} />}
           {!userName && <EntrarECadastroButtons button1Order={2} button2Order={1} className="gap-4" />}
           {showShoppingCart && <ShoppingCartButton />}
-          {userName && <div className="user_info_header">
-            <button className="image_user_button" style={{ backgroundImage: 'url(src/assets/user.svg)' }} />
-            {userName && <span className="user_name">Olá, {userName}</span>}
-          </div>}
+          {userName && (
+            <div className="user_info_header">
+              <button
+                className="image_user_button"
+                style={{ backgroundImage: 'url(src/assets/user.svg)' }}
+                onClick={toggleMenu}
+              />
+              <span className="user_name" onClick={toggleMenu}>Olá, {userName}</span>
+              {menuOpen && (
+                <div className="dropdown_menu" onBlur={closeMenu}>
+                  <ul>
+                    <li><Link to="/myprofile" className="text-decoration-none">Meu Perfil</Link></li>
+                    <li><button className="btn btn-link text-decoration-none p-0" onClick={handleLogout}>Sair</button></li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
         </div>
 
         {showHeader2 && (
